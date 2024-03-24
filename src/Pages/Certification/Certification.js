629fbd14cedd1001848fab36fe1f403e38f12fa2
Fakrul Islam <fislamc6@gmail.com>
2024-03-24 20:02:00 +0600

    certification page pdf added

--- src/Pages/Certification/Certification.js
+++ src/Pages/Certification/Certification.js
@@ -5,10 +5,16 @@ const Certification = () => {
     return (
         <div>
             <h4 className="text-center">Please here my Certification</h4>
-            <div className="container">
+            <div className="container d-flex flex-column">
                 <h4>Academic Certification</h4>
                 <p>
-                    <a className='certificate' href="https://drive.google.com/file/d/1ppWnG49EOZgQ0jiEjVZaeP0Y7eGL3Qmy/view?usp=drive_link" target="_blank" rel="noopener noreferrer">B.Sc. in Computer Science Engineering</a>
+                    <a className='certificate' href="https://drive.google.com/file/d/1ppWnG49EOZgQ0jiEjVZaeP0Y7eGL3Qmy/view" target="_blank" rel="noopener noreferrer">B.Sc. in Computer Science Engineering</a>
+                </p>
+                <p>
+                    <a className='certificate' href="https://drive.google.com/file/d/1AWW9TQNdVudf2ZvjU5Zy0zbmOdsdPbTe/view" target="_blank" rel="noopener noreferrer">Diploma in Engineering</a>
+                </p>
+                <p>
+                    <a className='certificate' href="https://drive.google.com/file/d/1LIXhfTdTYXWZcmQM2uegjXLAA0i2j3bI/view" target="_blank" rel="noopener noreferrer">Secondary School Certificate(S.S.C)</a>
                 </p>
             </div>
         </div>
